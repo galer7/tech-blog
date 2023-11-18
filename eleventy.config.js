0cafec6cc07ef7ec5acd97749fbabc0a987951cc
galer7 <gabigaler50@gmail.com>
2023-11-18 11:39:49 +0200

    Add eleventy-plugin-toc

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const pluginTOC = require("eleventy-plugin-toc");
 
 const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
@@ -27,6 +28,7 @@ module.exports = function (eleventyConfig) {
 	// App plugins
 	eleventyConfig.addPlugin(pluginDrafts);
 	eleventyConfig.addPlugin(pluginImages);
+	eleventyConfig.addPlugin(pluginTOC);
 
 	// Official plugins
 	eleventyConfig.addPlugin(pluginRss);
